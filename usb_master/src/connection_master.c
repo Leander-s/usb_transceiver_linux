4decef6bf9c5a95685df2f6764f7cfdbdcb8375f
Leander <leander.funken@live.de>
2024-07-17 20:21:24 +0200

    testing the waters

--- usb_master/src/connection_master.c
+++ usb_master/src/connection_master.c
@@ -72,8 +72,7 @@ int sendToSlave(master *m) {
   int failcounter = 0;
   while (!ack) {
     printf("Sending %s\n", m->sendBuffer);
-    memset(m->sendBuffer, 'o', BUFFER_SIZE-1);
-    ssize_t n = write(m->connection, m->sendBuffer, BUFFER_SIZE);
+    ssize_t n = write(m->connection, m->sendBuffer, strlen(m->sendBuffer));
     printf("%lu sent\n", n);
     if (n == 0) {
       printf("Nothing was sent\n");
--- usb_slave/src/connection_slave.c
+++ usb_slave/src/connection_slave.c
@@ -31,8 +31,8 @@ int initConnection(const char *path) {
   tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
   tty.c_oflag &= ~OPOST;
   tty.c_oflag &= ~ONLCR;
-  tty.c_cc[VTIME] = 10;
-  tty.c_cc[VMIN] = 0;
+  tty.c_cc[VTIME] = 0;
+  tty.c_cc[VMIN] = 10;
 
   cfsetspeed(&tty, B9600);
 
@@ -60,9 +60,10 @@ void destroySlave(slave *s) {
 int receiveData(slave *s) {
   ssize_t n = 0;
   while (n < BUFFER_SIZE) {
-    ssize_t received = read(s->connection, s->readBuffer, BUFFER_SIZE);
+    ssize_t received = read(s->connection, s->readBuffer, 10);
     n += received;
     if (received > 0) {
+      printf("%lu\n", received);
       printf("%s\n", s->readBuffer);
     }
     if (n == 0) {
