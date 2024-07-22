0350db0cfc3995ef7c02d3d19137c965d0e4c5b8
Leander <leander.funken@live.de>
2024-07-22 11:50:35 +0200

    starting windows support

--- usb_master/src/config.h
+++ usb_master/src/config.h
@@ -1,3 +1,8 @@
+#ifdef _WIN32
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#else
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,5 +10,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#endif
 
 #define BUFFER_SIZE 1024
--- usb_master/src/connection_master.c
+++ usb_master/src/connection_master.c
@@ -1,5 +1,71 @@
 #include "connection_master.h"
 
+#ifdef _WIN32
+int initConnection(const char* path){
+    HANDLE hComm = CreateFile(L"\\\\.\\COM12",                // Port name
+        GENERIC_READ | GENERIC_WRITE, // Read/Write
+        0,                            // No sharing
+        NULL,                         // No security
+        OPEN_EXISTING,                // Open existing port only
+        0,                            // Non-overlapped I/O
+        NULL);                        // Null for Comm devices
+
+    if (hComm == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error opening COM port" << std::endl;
+        return 1;
+    }
+
+    // Set COM port parameters
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+
+    if (!GetCommState(hComm, &dcbSerialParams)) {
+        std::cerr << "Error getting COM port state" << std::endl;
+        CloseHandle(hComm);
+        return 1;
+    }
+
+    dcbSerialParams.BaudRate = CBR_115200;  // Baud rate
+    dcbSerialParams.ByteSize = 8;         // Byte size
+    dcbSerialParams.StopBits = ONESTOPBIT;// Stop bits
+    dcbSerialParams.Parity = NOPARITY;    // Parity
+
+    if (!SetCommState(hComm, &dcbSerialParams)) {
+        std::cerr << "Error setting COM port state" << std::endl;
+        CloseHandle(hComm);
+        return 1;
+    }
+
+    // Set COM port timeouts
+    COMMTIMEOUTS timeouts = { 0 };
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 50;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+
+    if (!SetCommTimeouts(hComm, &timeouts)) {
+        std::cerr << "Error setting COM port timeouts" << std::endl;
+        CloseHandle(hComm);
+        return 1;
+    }
+    return hComm;
+}
+
+int writePort(HANDLE port, char* buffer){
+    int amount;
+    if(!WriteFile(port, buffer, strlen(buffer), &amount, NULL)){
+        return -1;
+    }
+    return amount;
+}
+
+int readPort(HANDLE port, char* buffer, int amount){
+    int amount;
+    if(!ReadFile(port, buffer, strlen(buffer), &amount, NULL)){
+        return -1;
+    }
+    return amount;
+}
+#else
 int initConnection(const char *path) {
   int port = open(path, O_RDWR);
   if (port < 0) {
@@ -42,6 +108,15 @@ int initConnection(const char *path) {
   return port;
 }
 
+int writePort(int port, char* buffer){
+    return write(port, buffer, strlen(buffer));
+}
+
+int readPort(int port, char* buffer, int amount){
+    return read(port, buffer, amount);
+}
+#endif
+
 master *createMaster() {
   master *result = (master *)malloc(sizeof(master));
   result->connection = initConnection("/dev/ttyACM0");
@@ -63,7 +138,7 @@ int sendToSlave(master *m) {
   int ack = 0;
   int failcounter = 0;
   while (!ack) {
-    ssize_t n = write(m->connection, m->sendBuffer, strlen(m->sendBuffer));
+    ssize_t n = writePort(m->connection, m->sendBuffer);
     if (n == 0) {
       printf("Nothing was sent\n");
       memset(m->readBuffer, '\0', BUFFER_SIZE);
@@ -73,7 +148,7 @@ int sendToSlave(master *m) {
     n = 0;
     int read_failcounter = 0;
     while (n < BUFFER_SIZE) {
-      ssize_t received = read(m->connection, m->readBuffer + n, BUFFER_SIZE);
+      ssize_t received = readPort(m->connection, m->readBuffer + n, BUFFER_SIZE);
       n += received;
       char last_c = m->readBuffer[strlen(m->readBuffer) - 1];
       if (last_c == '\n') {
